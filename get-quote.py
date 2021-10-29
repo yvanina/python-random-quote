8d201b54f18602791589d094c2c188be5cf960da
Vanina Yordanova <vanina.yordanova@nl.bosch.com>
2021-10-29 17:39:16 +0200

    Not perfect file writing and adding quotes programmatically

--- get-quote.py
+++ get-quote.py
@@ -16,8 +16,16 @@ def primary():
     counter+=1
     rnd1 = random.randint(0, last)
     print("Quote " + str(counter) + " of the day is: " + quotes[rnd1], end = "")
+
     #print("The quote" + str(counter) + "of the day is: " + quotes[rnd2])
 
+  quest = input ("Teach your bot a quote by typing it here:")
+  
+  with open("quotes.txt", "a") as f:
+    f.seek(0)
+    if len(quotes) > 0:
+      f.write("\n")
+      f.write(quest)
 
 
 if __name__== "__main__":
