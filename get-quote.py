f1d407f042128dca70d1f59b899bbb3368ad795a
Vanina Yordanova <vanina.yordanova@nl.bosch.com>
2021-10-29 16:47:42 +0200

    Bot that gives 13 quotes a day

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,22 @@
 import random
 
 def primary():
-  print("Keep it logically awesome.")
+  print("Welcome to my bot!")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
   
   last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  
+  #rnd2 = random.randint(0, last)
+
+  counter = 0
+  for counter in range(0,last):
+    counter+=1
+    rnd1 = random.randint(0, last)
+    print("Quote " + str(counter) + " of the day is: " + quotes[rnd1])
+    #print("The quote" + str(counter) + "of the day is: " + quotes[rnd2])
 
 if __name__== "__main__":
   primary()
