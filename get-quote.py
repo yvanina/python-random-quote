2f34da354eb7b4ab8cd4867287fe57f96dd20de0
Vanina Yordanova <vanina.yordanova@nl.bosch.com>
2021-10-29 16:58:55 +0200

    Print without \n, using end =  as a par at the end of print function

--- get-quote.py
+++ get-quote.py
@@ -15,8 +15,10 @@ def primary():
   for counter in range(0,last):
     counter+=1
     rnd1 = random.randint(0, last)
-    print("Quote " + str(counter) + " of the day is: " + quotes[rnd1])
+    print("Quote " + str(counter) + " of the day is: " + quotes[rnd1], end = "")
     #print("The quote" + str(counter) + "of the day is: " + quotes[rnd2])
+
+
 
 if __name__== "__main__":
   primary()
