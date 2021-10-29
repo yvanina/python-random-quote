324b1a5a13f9e6c5c51d2d6ceb8b57210c36302c
Vanina Yordanova <vanina.yordanova@nl.bosch.com>
2021-10-29 19:29:20 +0200

    Fix the adding new line to text file bug (See comments)

--- get-quote.py
+++ get-quote.py
@@ -15,12 +15,13 @@ def primary():
   for counter in range(0,last):
     counter+=1
     rnd1 = random.randint(0, last)
-    print("Quote " + str(counter) + " of the day is: " + quotes[rnd1], end = "")
+    print("Quote " + str(counter) + " of the day is: " + quotes[rnd1])
 
     #print("The quote" + str(counter) + "of the day is: " + quotes[rnd2])
 
-  quest = input ("Teach your bot a quote by typing it here:")
+  quest = input ("Teach your bot a quote by typing it here:\n")
   
+  #you ahve to fix this. currently there are 2 extra lines, before the new lines a added. 
   with open("quotes.txt", "a") as f:
     f.seek(0)
     if len(quotes) > 0:
